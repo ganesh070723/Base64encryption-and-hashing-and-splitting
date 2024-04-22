95cc7d023b61a566761e512da5a16e73064ffe12
ganesh070723 <bbabu8342@gmail.com>
2024-04-22 13:28:33 +0530

    host-update

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(8i45l--@esfnhsfq@=p=3fezcl291d=y-hlthbhew4uvrm=zg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
